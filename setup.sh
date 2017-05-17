a89605839aedcd7c656e4a8bcc3e909f8b353896
Heinrich Schuchardt <xypron.glpk@gmx.de>
2017-05-17 16:49:33 +0000

    setup.sh: add sudo user
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- setup.sh
+++ setup.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
-echo root:odroid | chpasswd
 apt-get update
 apt-get install xypron-keyring --allow-unauthenticated -y
 apt-get update
+apt-get install less locales sudo vim -y
 apt-get install flash-kernel -y
 apt-get install odroid-c2-u-boot-image -y
 apt-get install odroid-c2-kernel-image --reinstall -y
+adduser odroid --uid 9997 --gid 9997 --disabled-password \
+  --gecos 'Default User,,,'
+echo odroid:odroid | chpasswd
+adduser odroid sudo
