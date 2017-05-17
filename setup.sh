e86bf8446759f5e5ec56f4ed9a2c2de61b2978c9
Heinrich Schuchardt <xypron.glpk@gmx.de>
2017-05-17 18:44:03 +0000

    setup.sh: add ssh package
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 apt-get update
 apt-get install xypron-keyring --allow-unauthenticated -y
 apt-get update
-apt-get install less locales sudo vim -y
+apt-get install less locales ssh sudo vim -y
 apt-get install flash-kernel -o Dpkg::Options::="--force-confold" -y
 apt-get install odroid-c2-u-boot-image -y
 apt-get install odroid-c2-kernel-image --reinstall -y
