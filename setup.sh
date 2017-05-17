8f964219530148ec94e0275a505f5039eb533c73
Heinrich Schuchardt <xypron.glpk@gmx.de>
2017-05-17 17:35:30 +0000

    setup.sh: use copied flash-kernel configuration file
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- setup.sh
+++ setup.sh
@@ -3,10 +3,9 @@ apt-get update
 apt-get install xypron-keyring --allow-unauthenticated -y
 apt-get update
 apt-get install less locales sudo vim -y
-apt-get install flash-kernel -y
+apt-get install flash-kernel -o Dpkg::Options::="--force-confold" -y
 apt-get install odroid-c2-u-boot-image -y
 apt-get install odroid-c2-kernel-image --reinstall -y
-adduser odroid --uid 9997 --disabled-password \
-  --gecos 'Default User,,,'
+adduser odroid --uid 9997 --disabled-password --gecos 'Default User,,,'
 echo odroid:odroid | chpasswd
 adduser odroid sudo
