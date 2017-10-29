2d6abdac1daccd3928787024170d32e6bc8cfdbd
Heinrich Schuchardt <xypron.glpk@gmx.de>
2017-10-29 09:29:38 +0100

    Use Debian kernel
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ apt-get update
 apt-get install less locales ssh sudo vim -y
 apt-get install flash-kernel -y
 apt-get install odroid-c2-u-boot-image -y
-apt-get install odroid-c2-kernel-image --reinstall -y
+apt-get install linux-image-arm64 --reinstall -y
 adduser odroid --uid 9997 --disabled-password --gecos 'Default User,,,'
 echo odroid:odroid | chpasswd
 adduser odroid sudo
