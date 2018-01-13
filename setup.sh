f4c67134ee1d204bff4c42f882a72053a0f33524
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-01-13 16:13:56 +0100

    setup.sh: add bash-completion
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ apt-get update
 apt-get install xypron-keyring --allow-unauthenticated -y
 apt-get update
 apt-get install less locales ssh sudo vim ca-certificates open-iscsi dnsutils \
-  apt-file ntp -y
+  apt-file ntp bash-completion -y
 apt-get install flash-kernel -y
 apt-get install odroid-c2-u-boot-image -y
 apt-get install linux-image-arm64 --reinstall -y
