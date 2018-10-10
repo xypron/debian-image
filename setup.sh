9b3301982ca134e32eebc9db51d35fcc19485d98
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-10-10 08:28:51 +0200

    setup.sh: make installation non-interactive
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
+DEBIAN_FRONTEND=noninteractive
 apt-get update --allow-insecure-repositories
-apt-get install xypron-keyring --allow-unauthenticated -y
+apt-get install -yq xypron-keyring --allow-unauthenticated
 apt-get update
-apt-get install apt-file bash-completion ca-certificates dnsutils \
-  grub-efi-arm64 less locales ntp man open-iscsi ssh sudo vim -y
-apt-get install flash-kernel -y
-apt-get install odroid-c2-u-boot-image -y
-apt-get install linux-image-arm64 --reinstall -y
+apt-get install -yq apt-file bash-completion ca-certificates dnsutils \
+  flash-kernel grub-efi-arm64 less locales ntp man open-iscsi ssh sudo vim
+apt-get install -yq odroid-c2-u-boot-image
+apt-get install -yq linux-image-arm64 --reinstall
 adduser odroid --uid 9997 --disabled-password --gecos 'Default User,,,'
 echo odroid:odroid | chpasswd
 adduser odroid sudo
