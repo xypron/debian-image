944b1a8f14499763d4bf2373bdbf576a76a7cfaf
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-10-10 09:52:10 +0200

    Install flash-kernel non-interactively
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
-DEBIAN_FRONTEND=noninteractive
+export DEBIAN_FRONTEND=noninteractive
 apt-get update --allow-insecure-repositories
-apt-get install -yq xypron-keyring --allow-unauthenticated
+apt-get install -y xypron-keyring --allow-unauthenticated
 apt-get update
-apt-get install -yq apt-file bash-completion ca-certificates dnsutils \
-  flash-kernel grub-efi-arm64 less locales ntp man open-iscsi ssh sudo vim
-apt-get install -yq odroid-c2-u-boot-image
-apt-get install -yq linux-image-arm64 --reinstall
+apt-get install -y apt-file bash-completion ca-certificates dnsutils \
+  grub-efi-arm64 less locales ntp man open-iscsi ssh sudo vim
+apt-get install -y -o Dpkg::Options::="--force-confold" flash-kernel
+apt-get install -y odroid-c2-u-boot-image
+apt-get install -y linux-image-arm64 --reinstall
 adduser odroid --uid 9997 --disabled-password --gecos 'Default User,,,'
 echo odroid:odroid | chpasswd
 adduser odroid sudo
