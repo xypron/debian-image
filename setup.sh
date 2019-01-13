42a9299d49002d6904fd4ceda5328c4c5982ddb2
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-01-13 18:57:19 +0100

    setup.sh: specify correct packages
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 export DEBIAN_FRONTEND=noninteractive
 apt-get install -y apt-file bash-completion ca-certificates dnsutils \
-  grub-efi-arm64 less locales ntp man open-iscsi ssh sudo vim
+  grub-efi-arm less locales ntp man open-iscsi ssh sudo vim
 apt-get install -y -o Dpkg::Options::="--force-confold" flash-kernel
 apt-get install -y u-boot-sunxi
-apt-get install -y linux-image-arm64 --reinstall
+apt-get install -y linux-image-armmp-lpae --reinstall
 dpkg-reconfigure --frontend=noninteractive locales
 update-locale LANG="en_US.UTF-8" LANGUAGE="en_US.UTF-8" LC_ALL="en_US.UTF-8"
 adduser banana --uid 9997 --disabled-password --gecos 'Default User,,,'
