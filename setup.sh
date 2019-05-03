ce50e5cfd7c9e2c380f3029a245eebedc6146996
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-05-03 19:41:06 +0200

    setup.sh: use --force-confold for raspi3-firmware
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y apt-file bash-completion ca-certificates dnsutils \
   less locales ntp man open-iscsi ssh sudo vim
-apt-get install -y raspi3-firmware
+apt-get install -y -o Dpkg::Options::="--force-confold" raspi3-firmware
 apt-get install -y linux-image-arm64 --reinstall
 dpkg-reconfigure --frontend=noninteractive locales
 update-locale LANG="en_US.UTF-8" LANGUAGE="en_US.UTF-8" LC_ALL="en_US.UTF-8"
