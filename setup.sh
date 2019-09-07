d72f11c3cb1d0aad0445458187639f5a6a7c1250
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-09-07 08:56:06 +0200

    Use linux-image-armmp-lpae kernel
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ apt-get update
 apt-get install -y apt-file bash-completion ca-certificates dnsutils \
   less locales ntp man open-iscsi ssh sudo vim
 apt-get install -y -o Dpkg::Options::="--force-confold" raspi3-firmware
-apt-get install -y linux-image-arm64 --reinstall
+apt-get install -y linux-image-armmp-lpae --reinstall
 dpkg-reconfigure --frontend=noninteractive locales
 update-locale LANG="en_US.UTF-8" LANGUAGE="en_US.UTF-8" LC_ALL="en_US.UTF-8"
 adduser rpi3 --uid 9997 --disabled-password --gecos 'Default User,,,'
